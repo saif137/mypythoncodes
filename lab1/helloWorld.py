79db5bedf2f2f5e7474c292374733efaf53e4b36
saif <saif.rahman@sse.habib.edu.pk>
2016-08-16 11:05:01 +0500

    Hello world program to check if every thing is working on systems

---
+++ lab1/helloWorld.py
@@ -0,0 +1,12 @@
+#! C:\Python27
+
+def say_hello(name):
+    """say_hello: string -> .
+    Consumes: a string, name
+    Produces: nothing
+    Purpose: prints a greeting of the form "Hello <name>!"
+    """
+    print "Hello " + name + "!" #this is the function body
+
+if __name__ == "__main__":
+    say_hello("Dr. Syed Saif ur Rahman")
